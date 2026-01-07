6091d9ac0c624d253bd24fe731c74b3724593ef5
venkataprasad2024 <pandillavenkataprasad@gmail.com>
2026-01-07 23:06:23 +0530

    changes made

--- ADVANCED/Mini_Project/todo.js
+++ ADVANCED/Mini_Project/todo.js
@@ -4,8 +4,13 @@ let input=document.getElementById("taskInput");
 
 btn.addEventListener("click",function(){
   let item=document.createElement("li");
+  let deltbtn=document.createElement("button");
+  deltbtn.textContent="Delete";
+
   item.textContent=input.value;
+    item.appendChild(deltbtn);
   ul.appendChild(item);
     console.log(input.value);
     input.value="";
 })
+
